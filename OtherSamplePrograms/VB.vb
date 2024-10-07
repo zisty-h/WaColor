3574c55d9657d5acbae51779424386b8e9401f50
Syobosyobonn <154320513+syobosyobonn@users.noreply.github.com>
2024-10-07 20:59:10 +0900

    (`・ω・´)

--- OtherSamplePrograms/VB.vb
+++ OtherSamplePrograms/VB.vb
@@ -2,26 +2,26 @@ Imports System.Drawing
 
 Public Structure WaColor
     Public Structure Macha
-        Public Shared Light As Color = Color.FromArgb(67, 178, 67)  ' #43B243
-        Public Shared Normal As Color = Color.FromArgb(0, 119, 0)    ' #007700
-        Public Shared Deep As Color = Color.FromArgb(0, 64, 0)       ' #004000
+        Public Shared Light As Color     = Color.FromArgb(67, 178, 67)  ' #43B243
+        Public Shared Normal As Color    = Color.FromArgb(0, 119, 0)    ' #007700
+        Public Shared Deep As Color      = Color.FromArgb(0, 64, 0)     ' #004000
     End Structure
 
     Public Structure Azuki
-        Public Shared Light As Color = Color.FromArgb(117, 0, 0)      ' #750000
-        Public Shared Normal As Color = Color.FromArgb(135, 51, 51)    ' #873333
-        Public Shared Deep As Color = Color.FromArgb(158, 67, 67)      ' #9E4343
+        Public Shared Light As Color     = Color.FromArgb(117, 0, 0)    ' #750000
+        Public Shared Normal As Color    = Color.FromArgb(135, 51, 51)  ' #873333
+        Public Shared Deep As Color      = Color.FromArgb(158, 67, 67)  ' #9E4343
     End Structure
 
     Public Structure Kinako
-        Public Shared Light As Color = Color.FromArgb(150, 142, 0)     ' #968E00
-        Public Shared Normal As Color = Color.FromArgb(183, 176, 33)    ' #B7B021
-        Public Shared Deep As Color = Color.FromArgb(216, 210, 82)      ' #D8D252
+        Public Shared Light As Color     = Color.FromArgb(216, 210, 82) ' #D8D252
+        Public Shared Normal As Color    = Color.FromArgb(183, 176, 33) ' #B7B021
+        Public Shared Deep As Color      = Color.FromArgb(150, 142, 0)  ' #968E00
     End Structure
 
     Public Structure Sora
-        Public Shared Light As Color = Color.FromArgb(0, 99, 114)       ' #006372
-        Public Shared Normal As Color = Color.FromArgb(0, 139, 160)      ' #008BA0
-        Public Shared Deep As Color = Color.FromArgb(69, 186, 204)       ' #45BACC
+        Public Shared Light As Color     = Color.FromArgb(69, 186, 204)  ' #45BACC
+        Public Shared Normal As Color    = Color.FromArgb(0, 139, 160)   ' #008BA0
+        Public Shared Deep As Color      = Color.FromArgb(0, 99, 114)    ' #006372
     End Structure
 End Structure
