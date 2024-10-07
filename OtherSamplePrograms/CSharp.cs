c237424680d1d88cc2b1714735acdb48f075f281
Syobosyobonn <154320513+syobosyobonn@users.noreply.github.com>
2024-10-07 20:56:48 +0900

    (`・ω・´)

--- OtherSamplePrograms/CSharp.cs
+++ OtherSamplePrograms/CSharp.cs
@@ -18,15 +18,15 @@ public struct WaColor
 
     public struct Kinako
     {
-        public static Color Light   = Color.FromArgb(150, 142, 0);  // #968E00
+        public static Color Light   = Color.FromArgb(216, 210, 82); // #D8D252
         public static Color Normal  = Color.FromArgb(183, 176, 33); // #B7B021
-        public static Color Deep    = Color.FromArgb(216, 210, 82); // #D8D252
+        public static Color Deep    = Color.FromArgb(150, 142, 0);  // #968E00
     }
 
     public struct Sora
     {
-        public static Color Light   = Color.FromArgb(0, 99, 114);   // #006372
+        public static Color Light   = Color.FromArgb(69, 186, 204); // #45BACC
         public static Color Normal  = Color.FromArgb(0, 139, 160);  // #008BA0
-        public static Color Deep    = Color.FromArgb(69, 186, 204); // #45BACC
+        public static Color Deep    = Color.FromArgb(0, 99, 114);   // #006372
     }
 }
